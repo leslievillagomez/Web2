a45a828043c175f705fb823e24a7968f8c1e85ab
leslievillagomez <leslievillagomez22@aragon.unam.mx>
2025-08-27 17:33:56 -0600

    Actualizado

--- leslieProgramas/Web2/26ago25/app.js
+++ leslieProgramas/Web2/26ago25/app.js
@@ -12,13 +12,3 @@ console.log("Suma = "+ suma,
     "Multiplicacion = " + multi, 
     "Division = " + div, 
     "Modulo = " + modulo);
-
-// let variable1 = parseFloat (prompt("ingresa primer valor"));
-// let variable2 = parseFloat (prompt("ingresa segundo valor"));
-// let sum = variable1 + variable2;
-// let rest = variable1 - variable2;
-// let mul =  variable1 * variable2;
-// let div =  variable1 / variable2;
-// console.log("Resultado de suma es: " + sum);
-// console.log("Resultado de resta es: " + rest);
-// console.log("Resultado de multiplicacion es: " + mul);
