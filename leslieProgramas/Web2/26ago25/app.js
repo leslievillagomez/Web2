ed76b7cb3ba2d7b126e4f8008c66ff0e9cba372a
leslievillagomez <leslievillagomez22@aragon.unam.mx>
2025-08-27 17:24:13 -0600

    Operaciones en consola

---
+++ leslieProgramas/Web2/26ago25/app.js
@@ -0,0 +1,24 @@
+let num1 = parseFloat (prompt ("Ingrese el primer numero"));
+let num2 = parseFloat (prompt ("Ingrese el segundo numero"));
+
+let suma = num1 + num2;
+let resta = num1 - num2;
+let multi = num1 * num2;
+let div = num1 / num2;
+let modulo = num1 % num2;
+
+console.log("Suma = "+ suma, 
+    "Resta = " + resta, 
+    "Multiplicacion = " + multi, 
+    "Division = " + div, 
+    "Modulo = " + modulo);
+
+// let variable1 = parseFloat (prompt("ingresa primer valor"));
+// let variable2 = parseFloat (prompt("ingresa segundo valor"));
+// let sum = variable1 + variable2;
+// let rest = variable1 - variable2;
+// let mul =  variable1 * variable2;
+// let div =  variable1 / variable2;
+// console.log("Resultado de suma es: " + sum);
+// console.log("Resultado de resta es: " + rest);
+// console.log("Resultado de multiplicacion es: " + mul);
